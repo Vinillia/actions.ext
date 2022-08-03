3b850d85e8dab141c64f69b8e1f95ed0e83f6c44
Ei <51745975+Vinillia@users.noreply.github.com>
2022-08-03 18:32:17 +1200

    Egg

--- cmake/sourcemod.cmake
+++ cmake/sourcemod.cmake
@@ -31,12 +31,12 @@ function(add_extension ext_name engine)
 	target_compile_definitions(${ext_name} PUBLIC SOURCE_ENGINE=${engine})
 
 	target_sources(${ext_name} PUBLIC
-	${CMAKE_CURRENT_SOURCE_DIR}/source/public/extension.cpp
-	${CMAKE_CURRENT_SOURCE_DIR}/source/actions/public/actions_propagate.cpp
-	${CMAKE_CURRENT_SOURCE_DIR}/source/actions/public/actions_manager.cpp
-	${CMAKE_CURRENT_SOURCE_DIR}/source/actions/public/actions_helpers.cpp
-	${CMAKE_CURRENT_SOURCE_DIR}/actions-sdk/source/sdk/public/actions_cquery.cpp
-	${CMAKE_CURRENT_SOURCE_DIR}/actions-sdk/source/sdk/public/actions_processor.cpp
+	${CMAKE_CURRENT_SOURCE_DIR}/actions-ext/source/public/extension.cpp
+	${CMAKE_CURRENT_SOURCE_DIR}/actions-ext/source/actions/public/actions_propagate.cpp
+	${CMAKE_CURRENT_SOURCE_DIR}/actions-ext/source/actions/public/actions_manager.cpp
+	${CMAKE_CURRENT_SOURCE_DIR}/actions-ext/source/actions/public/actions_helpers.cpp
+	${CMAKE_CURRENT_SOURCE_DIR}/source/sdk/public/actions_cquery.cpp
+	${CMAKE_CURRENT_SOURCE_DIR}/source/sdk/public/actions_processor.cpp
 	${SM_PATH}/public/smsdk_ext.cpp)
 
 	target_include_directories(${ext_name} PUBLIC 
