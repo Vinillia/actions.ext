fa4559078f466dfdbe06b57ec8c2e86c0d3d7266
Ei <sweetiesense@icloud.com>
2023-05-12 19:40:23 +1200

    👺

--- source/actions_pubvars.h
+++ source/actions_pubvars.h
@@ -58,7 +58,8 @@ namespace compile
 	template<const std::string_view&... strs>
 	struct concat_t
 	{
-		static constexpr auto _concat() noexcept
+		// https://stackoverflow.com/questions/38955940/how-to-concatenate-static-strings-at-compile-time
+		static constexpr auto impl() noexcept
 		{
 			constexpr std::size_t len = (strs.size() + ... + 0);
 			std::array<char, len + 1> arr{};
@@ -71,7 +72,9 @@ namespace compile
 			return arr;
 		}
 
-		static constexpr auto arr = _concat();
+		// Give the joined string static storage
+		static constexpr auto arr = impl();
+		// View as a std::string_view
 		static constexpr std::string_view value{ arr.data(), arr.size() - 1 };
 	};
 
