2aeb570859c01c740175865bc5e9af5fba7a8dc6
Ei <sweetiesense@icloud.com>
2025-06-08 14:49:54 +0300

    exclude linux content from windows fix VS windows build

--- source/sdk/l4d/actions_tools_l4d.cpp
+++ source/sdk/l4d/actions_tools_l4d.cpp
@@ -2,12 +2,17 @@
 #include "actions_tools_l4d.h"
 #include "NextBotComponentInterface.h"
 
-#include <cxxabi.h>
+#include <am-platform.h>
 
 #include "../../l4d2/actions_tools_l4d2.cpp"
 
+#if defined(KE_LINUX)
+#include <cxxabi.h>
+#endif
+
 L4DActionsTools g_L4DActionsTools;
 
+#if defined(KE_LINUX)
 std::string Demangle(const char* mangled) 
 {
     int status = 0;
@@ -22,8 +27,13 @@ bool IsIIntention(INextBotComponent* i)
     std::string name = Demangle(typeid(*i).name());
     return name.find("Intention") != std::string::npos;
 }
+#endif
 
 IIntention* L4DActionsTools::TryCastToIntentionComponent(INextBotComponent* component)
 {
+#if defined(KE_LINUX)
 	return IsIIntention(component) ? reinterpret_cast<IIntention*>(component) : nullptr;
+#else
+	return ActionsTools::TryCastToIntentionComponent(component);
+#endif
 }
